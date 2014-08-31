71751905409b5363a0fadc1b8c6ce16a78e325c2
Mahram Z. Foadi <mahram@mahram.ca>
2014-08-31 13:36:28 -0700

    mkvendor.sh: Initial commit of g3

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := g3
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/g3/device_g3.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := g3
+PRODUCT_NAME := cm_g3
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := g3
+PRODUCT_MANUFACTURER := lge
